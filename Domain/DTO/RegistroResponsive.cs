94db67eb825655866dbed412c6efed590cf626b1
Charly21393157 <21393157@utcancun.edu.mx>
2024-06-03 20:06:01 -0500

    Agregar archivos de proyecto.

---
+++ Domain/DTO/RegistroResponsive.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class RegistroResponsive
+    {
+        public string Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Cantidad { get; set; }
+        public int FkTipo { get; set; }
+    }
+}
---
+++ Domain/Entities/Registro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Registro
+    {
+        [Key]
+        public int PkRegistro { get; set; }
+        public string Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Cantidad { get; set; }
+
+        [ForeignKey("Tipos")]
+        public int FkTipo { get; set; }
+        public Tipo Tipos { get; set; }
+    }
+}
